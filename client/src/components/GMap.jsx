38c2c4760bf51be0818157d4deaad0e761028252
Andy Dam <andy@andydam.com>
2018-01-16 15:37:58 -0800

    (bug) remove backend dependency from frontend (#4)

--- client/src/components/GMap.jsx
+++ client/src/components/GMap.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-import API from './../../../api/env.js';
+// import API from './../../../api/env.js';
 import GoogleMapReact from 'google-map-react';
 import Marker from './Marker.jsx';
 
-const MapContainer = ({ latLong }) => <div><Marker /></div>;
+const MapContainer = ({ latLong }) => (
+  <div>
+    <Marker />
+  </div>
+);
 
 export default class GMap extends React.Component {
   constructor(props) {
@@ -12,21 +16,21 @@ export default class GMap extends React.Component {
     this.state = {
       latLong: this.props.latLong || { lat: 42.2828747, lng: -71.13467840000001 },
       zoom: 15,
-      apiKey: API.GMapKey,
+      apiKey: '',
       style: {
-      width: '100vw',
-      height: '60vh',
-      }
-    }
+        width: '100vw',
+        height: '60vh',
+      },
+    };
   }
 
   render() {
     return (
-      <div className='google-map' style={this.state.style}>
+      <div className="google-map" style={this.state.style}>
         <GoogleMapReact
-          bootstrapURLKeys={ {key: this.state.apiKey} } 
-          defaultCenter={ this.state.latLong }
-          defaultZoom={ this.state.zoom }
+          bootstrapURLKeys={{ key: this.state.apiKey }}
+          defaultCenter={this.state.latLong}
+          defaultZoom={this.state.zoom}
           defaultOptions={{
             streetViewControl: false,
             scaleControl: false,
@@ -34,11 +38,11 @@ export default class GMap extends React.Component {
             panControl: false,
             zoomControl: false,
             rotateControl: false,
-            fullscreenControl: false
-          }} disableDefaultUI>
-          <MapContainer 
-            latLong={this.state.latLong}
-          />
+            fullscreenControl: false,
+          }}
+          disableDefaultUI
+        >
+          <MapContainer latLong={this.state.latLong} />
         </GoogleMapReact>
       </div>
     );
